f41ceca0ba7c7410f11d4b4c9c3f97cd6b8ab437
mdhcodes <mdhcodes@gmail.com>
2024-06-13 14:45:21 -0400

    Add templates and static folders and urls.py file to virtualVisit app

--- virtualVisit/views.py
+++ virtualVisit/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from museums.settings import SI_API_KEY
+
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'virtualVisit/index.html')
+
+
